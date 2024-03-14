5766ff85d6c41a0c7f4be8ee501dc62cabb48db3
Yerukondu Haritha <yerukonduharitha@gmail.com>
2024-03-14 14:32:49 +0530

    Day7 work updated

---
+++ Day7/Part1/Question4.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Question4 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of rows :");
+    int rows = sc.nextInt();
+    System.out.print("How many times repeate this pattern in row wise :");
+    int noOfTimes = sc.nextInt();
+    for(int h=1;h<=noOfTimes;h++) {
+      for(int i = 1; i <= rows; i++) {
+        if(i == 1 || i == rows) {
+          for(int j = 1; j <= rows; j++) {
+            System.out.print("*");
+          }
+          System.out.println(); 
+        }
+        else if(i >= 2 && i < rows) { 
+          for(int j = 1; j <= rows; j++) {
+            if(j == 1 || j == rows) {
+              System.out.print("*");
+            }
+            else {
+              System.out.print(" ");
+            }
+          }
+          System.out.println(); 
+        }
+      }
+    }
+    
+  }
+  
+}
